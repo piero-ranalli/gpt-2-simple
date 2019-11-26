b7a0b063a91cb789f639936b5051222f522595ce
Piero Ranalli <piero.ranalli@combient.com>
2019-11-26 11:57:20 +0100

    .

--- gpt_2_simple/src/sample_store.py
+++ gpt_2_simple/src/sample_store.py
@@ -11,7 +11,7 @@ class SampleStore:
     def add_sample(self,text,model_name,iters,prefix=None):
         print("sstore add sample")
 
-        with open(self.storefile, 'w+') as csvfile:
+        with open(self.storefile, 'a+') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([model_name,iters,prefix,text])
         
